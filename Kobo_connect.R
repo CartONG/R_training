cba8dab52fb51a898ffcfde797f002d40248cbb3
AbelGelman <abel.gelman@gmail.com>
2021-06-08 16:05:59 +0200

    Add 2 different sources of web data: XLS/CSV, KOBO (OCHA data connection works, BUT UNHCR still fails)

--- Kobo_connect.R
+++ Kobo_connect.R
@@ -1,8 +1,41 @@
 ################################################################################
-## 1 - Download data from KoboToolBox
+## 1 - INTRODUCTION - Download data from the web
 ################################################################################
+## A - Example download file from the web
+
+library(utils)
+ 
+#download file and store in workspace
+download.file(
+  url = "https://www.ine.gob.gt/sistema/uploads/2016/10/28/0NiM1ouoHaN67SRO2IzXZ5RNI7FeyHpn.xls",
+  destfile = "guatemala_admin.xls"
+)
+
+
+library(readxl)
+guatemala_admin <- read_excel("guatemala_admin.xls", 
+                              sheet = "Departamentos y municipios")
+
+View(guatemala_admin)
+
+
+#download file and store in workspace
+download.file(
+  url = "https://www.downloadexcelfiles.com/sites/default/files/docs/list_of_municipalities_of_el_salvador-1736j.xlsx",
+  destfile = "SLV_admin.xlsx"
+)
+
+#upload file into the environment as object
+library(readxl)
+SLV_admin <- read_excel("SLV_admin.xlsx")
+View(SLV_admin)
+
+
+
+################################################################################
+## B - Download data from KoboToolBox - HTTT GET
 ## Source: https://github.com/ppsapkota/kobohr_apitoolbox
-################################################################################
+
 
 library(httr)
 library(tidyverse)
@@ -10,6 +43,8 @@ library(tidyverse)
 library(devtools)
 source_url("https://raw.githubusercontent.com/ppsapkota/kobohr_apitoolbox/master/R/r_func_ps_kobo_utils.R")
 
+
+# OCHA
 #Set kobo credentials --> in separate script
 
 #Download form/project list
@@ -21,11 +56,33 @@ view(d_formlist_csv)
 
 #Download data in CSV format
 
-form_id <- d_formlist_csv[24,2]# form ID of the data to download. Manually checked the if in view()
+form_id <- d_formlist_csv[24,2]# form ID of the data to download. Manually checked the id in view()
 
 url <- paste("https://kc.humanitarianresponse.info/api/v1/data/",form_id, ".csv", sep = "")
 d_raw <- kobohr_getdata_csv(url,kobo_user,kobo_pw)  
 data <- as.data.frame(d_raw)
 
+
+
+
+################################################################################
+## Source: https://humanitarian-user-group.github.io/post/kobo_restapi/
+################################################################################
+
+library(httr)
+library(jsonlite) # package for working with JSON data
+#for reading and writing data
+library(readr) # to read CSV data
+library(openxlsx) # to write to excel file
+
+# UNHCR
+# Set form ID from kobo 
+form_id<-"amAXvCQ2QbikAXoEh7SSuB" #2020 DRS Livelihoods demo survey
+
+download.file(
+  url = "https://kobo.unhcr.org/api/v2/assets/amAXvCQ2QbikAXoEh7SSuB.xls",
+  destfile = "liv_2020_demo"
+  
+)
 
 
--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 ## PRESENTATION OUTLINE 
 ### Introduction
 #### 1 - What is it?
+When it was created
+Main characteristics
+
+#### 2 - Applications
+Fields of applicatios: brief information by industry 
+Focus on humanitarian IM + GIS
+
+#### 3 - CRAN 
+("https://cran.r-project.org/") -> The Comprhensive R Archive Network: CRAN is a network of ftp and web servers around the world that store identical, up-to-date, versions of code and documentation for R. Please use the CRAN mirror nearest to you to minimize network load. 
+
+### 4 - Libraries 
+
+### 5 - R Studio IDE
+
+### Workflow
+#### Import: readr + haven + XLSX + DBI?
+
+
+### Ressources
+https://humanitarian-user-group.github.io
+This site hosts information related to the HumanitaRian-useR-group. This group is open to Humanitarian Information Management officers from any organisation. The initial group was created in November 2018 after a workshop in Amman funded by UNHCR.
